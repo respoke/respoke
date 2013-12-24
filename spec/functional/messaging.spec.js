0d37456d3ec1e36f8ed09c8005c59ee87bfc3b2c
Chad McElligott <cmcelligott@digium.com>
2013-12-24 17:03:41 -0600

    updating functional tests to be run using mocha

--- spec/functional/messaging.spec.js
+++ spec/functional/messaging.spec.js
@@ -2,16 +2,17 @@ var username1;
 var username2;
 var username3;
 var password = 'password';
-
+var expect = require('chai').expect;
 var helpers = require('../util/helpers.js');
 var Client = helpers.Client;
 
-
 var driver;
-var testFixture;
 var env;
 
 describe('Messaging', function () {
+
+    this.timeout(30000);
+
     var driver1;
     var client1;
     var client1Name = 'client1';
@@ -24,48 +25,45 @@ describe('Messaging', function () {
     var client3;
     var client3Name = 'client3';
 
-    it("fixture setup", function (done) {
+    before(function (done) {
         helpers.testFixtureBeforeTest({randomUserNames: 3, createContacts: true}, function (d, environment) {
             driver = d;
             env = environment;
             username1 = env.users[0].username;
             username2 = env.users[1].username;
             username3 = env.users[2].username;
-            done();
+
+            driver1 = helpers.webDriver();
+            client1 = new Client(driver1, client1Name, env.url);
+            driver1.get(process.env['MERCURY_URL'] + '/index.html');
+            client1.init(env.appId);
+            client1.connect();
+            client1.login(username1, password).then(function () {
+                driver2 = helpers.webDriver();
+                client2 = new Client(driver2, client2Name, env.url);
+                driver2.get(process.env['MERCURY_URL'] + '/index.html');
+                client2.init(env.appId);
+                client2.connect();
+                client2.login(username2, password);
+            }).then(function () {
+                    driver3 = helpers.webDriver();
+                    client3 = new Client(driver3, client3Name, env.url);
+                    driver3.get(process.env['MERCURY_URL'] + '/index.html');
+                    client3.init(env.appId);
+                    client3.connect();
+                    client3.login(username3, password);
+                }).then(function () {
+                    done();
+                });
         });
     });
-
-    it("test setup", function (done) {
-        driver1 = helpers.webDriver();
-        client1 = new Client(driver1, client1Name, env.url);
-        driver1.get(process.env['MERCURY_URL'] + '/index.html');
-        client1.init(env.appId);
-        client1.connect();
-        client1.login(username1, password).then(function () {
-            driver2 = helpers.webDriver();
-            client2 = new Client(driver2, client2Name, env.url);
-            driver2.get(process.env['MERCURY_URL'] + '/index.html');
-            client2.init(env.appId);
-            client2.connect();
-            client2.login(username2, password);
-        }).then(function () {
-            driver3 = helpers.webDriver();
-            client3 = new Client(driver3, client3Name, env.url);
-            driver3.get(process.env['MERCURY_URL'] + '/index.html');
-            client3.init(env.appId);
-            client3.connect();
-            client3.login(username3, password);
-        }).then(function () {
-            done();
-        });
-    }, 15000);
 
     describe("send and receive messages", function () {
 
         xit("should send message to contact", function (done) {
             client1.sendMessage(username2, 'test1').then(function () {
                 client1.getMessages(username2).then(function (messages) {
-                    expect(messages).toContain('test1');
+                    expect(messages).to.contain('test1');
                     done();
                 });
             });
@@ -81,25 +79,21 @@ describe('Messaging', function () {
 
             setTimeout(function () {
                 client2.getValue('messageReceived.getText()').then(function (value) {
-                    expect(value).toBe('testMessage-username1');
+                    expect(value).to.equal('testMessage-username1');
                     client3.getValue('messageReceived.getText()').then(function (value) {
-                        expect(value).toBe('testMessage-username1');
+                        expect(value).to.equal('testMessage-username1');
                         done();
                     });
                 });
             }, 1000);
 
-        }, 10000);
+        });
     });
 
-    it("test teardown", function (done) {
+    after(function (done) {
         driver1.quit();
         driver2.quit();
         driver3.quit();
-        done();
-    });
-
-    it("fixture teardown", function (done) {
         helpers.testFixtureAfterTest(driver, done);
     });
 });
--- spec/functional/presentable.spec.js
+++ spec/functional/presentable.spec.js
@@ -1,16 +1,17 @@
 var username1;
 var username2;
 var password = 'password';
-
+var expect = require('chai').expect;
 var helpers = require('../util/helpers.js');
 var Client = helpers.Client;
 
-
 var driver;
-var testFixture;
 var env;
 
 describe('Presence setup', function () {
+
+    this.timeout(30000);
+
     var driver1;
     var client1;
     var client1Name = 'client1';
@@ -19,33 +20,27 @@ describe('Presence setup', function () {
     var client2;
     var client2Name = 'client2';
 
-    it("fixture setup", function (done) {
+    before(function (done) {
         helpers.testFixtureBeforeTest({randomUserNames: 2, createContacts: true}, function (d, environment) {
             driver = d;
             env = environment;
             username1 = env.users[0].username;
             username2 = env.users[1].username;
-            done();
+            driver1 = helpers.webDriver();
+            client1 = new Client(driver1, client1Name, env.url);
+            driver1.get(process.env['MERCURY_URL'] + '/index.html')
+            client1.init(env.appId);
+            client1.connect();
+            client1.login(username1, password).then(function () {
+                driver2 = helpers.webDriver();
+                client2 = new Client(driver2, client2Name, env.url);
+                driver2.get(process.env['MERCURY_URL'] + '/index.html');
+                client2.init(env.appId);
+                client2.connect();
+            }).then(function () {
+                    done();
+                });
         });
-    });
-
-    it("test setup", function (done) {
-        driver1 = helpers.webDriver();
-        client1 = new Client(driver1, client1Name, env.url);
-        driver1.get(process.env['MERCURY_URL'] + '/index.html')
-        client1.init(env.appId);
-        client1.connect();
-        client1.login(username1, password).then(function () {
-            driver2 = helpers.webDriver();
-            client2 = new Client(driver2, client2Name, env.url);
-            driver2.get(process.env['MERCURY_URL'] + '/index.html');
-            client2.init(env.appId);
-            client2.connect();
-        }).then(function () {
-            done();
-        });
-
-
     });
 
     describe("Presence", function () {
@@ -54,69 +49,64 @@ describe('Presence setup', function () {
 
         it("#getPresence", function (done) {
             client1.getPresence().then(function (presence) {
-                expect(presence).toBe('available');
+                expect(presence).to.equal('available');
                 done();
             });
         });
 
         it("#getDisplayName", function (done) {
             client1.getDisplayName().then(function (name) {
-                expect(name).toBe(env.users[0].username);
+                expect(name).to.equal(env.users[0].username);
                 done();
             });
         });
 
         it("#getUsername", function (done) {
             client1.getUsername().then(function (name) {
-                expect(name).toBe(env.users[0].username);
+                expect(name).to.equal(env.users[0].username);
                 done();
             });
         });
 
         xit("#getName", function (done) {
             client1.getName().then(function (name) {
-                expect(name).toBe(env.users[0].username);
+                expect(name).to.equal(env.users[0].username);
                 done();
             });
         });
 
         xit("#getID", function (done) {
             client1.getID().then(function (id) {
-                expect(id).toBe(env.users[0].id);
+                expect(id).to.equal(env.users[0].id);
                 done();
             });
         });
 
         it("#canSendAudio", function (done) {
             client1.canSendAudio().then(function (b) {
-                expect(b).toBe(true);
+                expect(b).to.equal(true);
                 done();
             });
         });
 
         it("#canSendVideo", function (done) {
             client1.canSendVideo().then(function (b) {
-                expect(b).toBe(true);
+                expect(b).to.equal(true);
                 done();
             });
         });
 
         it("#callInProgress", function (done) {
             client1.callInProgress().then(function (b) {
-                expect(b).toBe(false);
+                expect(b).to.equal(false);
                 done();
             });
         });
     });
 
-    it("test teardown", function (done) {
+    after(function (done) {
         driver1.quit();
         driver2.quit();
-        done();
-    });
-
-    it("fixture teardown", function (done) {
         helpers.testFixtureAfterTest(driver, done);
     });
-
 });
