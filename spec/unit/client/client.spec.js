a1dfb46f4517c665f432ec0bda8a1d733f1f07d9
Erin Spiceland <espiceland@digium.com>
2014-05-19 17:00:27 -0500

    Add more tests for brightstream.Client.

--- spec/unit/client/client.spec.js
+++ spec/unit/client/client.spec.js
@@ -51,6 +51,16 @@ describe("brightstream.Client", function () {
     it("doesn't expose the signaling channel", function () {
         expect(client.signalingChannel).to.not.exist;
         expect(client.getSignalingChannel).to.not.exist;
+        Object.keys(client).forEach(function (key) {
+            expect(key).to.not.contain('signal');
+        });
+    });
+
+    it("has default getUserMedia constraints", function () {
+        expect(client.callSettings).to.be.an.Object;
+        expect(client.callSettings.constraints).to.be.an.Object;
+        expect(client.callSettings.constraints.video).to.be.true;
+        expect(client.callSettings.constraints.audio).to.be.true;
     });
 
     describe("when not connected", function () {
@@ -64,16 +74,16 @@ describe("brightstream.Client", function () {
                         done(new Error("User presence succeeded with no connection!"));
                     },
                     onError: function (err) {
-                        expect(err.message).to.contain("no connection");
+                        expect(err.message).to.contain("not connected");
                         done();
                     }
                 });
             });
         });
 
-        describe("connect() 1", function () {
+        describe("connect()", function () {
             describe("when called without developmentMode", function () {
-                it("throws an error 1", function (done) {
+                it("throws an error", function (done) {
                     client.connect({
                         appId: brightstream.makeGUID(),
                         endpointId: brightstream.makeGUID()
@@ -88,7 +98,7 @@ describe("brightstream.Client", function () {
             });
 
             describe("when called without appId", function () {
-                it("throws an error 2", function (done) {
+                it("throws an error", function (done) {
                     client.connect({
                         developmentMode: true,
                         endpointId: brightstream.makeGUID()
@@ -103,7 +113,7 @@ describe("brightstream.Client", function () {
             });
 
             describe("when called without endpointId", function () {
-                it("throws an error 3", function (done) {
+                it("throws an error", function (done) {
                     client.connect({
                         appId: brightstream.makeGUID(),
                         developmentMode: true
@@ -115,6 +125,106 @@ describe("brightstream.Client", function () {
                         done();
                     });
                 });
+            });
+        });
+
+        describe("disconnect()", function () {
+            it("doesn't change the client.connected flag", function () {
+                var oldConnected = client.connected;
+                expect(oldConnected).to.be.false;
+                client.disconnect();
+                expect(oldConnected).to.equal(client.connected);
+            });
+
+            // Disabled because it doesn't work with the minified library.
+            xit("doesn't fire the disconnect event", function (done) {
+                sinon.spy(client, 'fire');
+                client.disconnect();
+                setTimeout(function () {
+                    expect(client.fire.notCalled).to.be.defined;
+                    if (client.fire.getCall(0)) {
+                        expect(client.fire.getCall(0).args[0]).to.be.ok;
+                        console.log(client.fire.getCall(0).args);
+                    }
+                    client.fire.restore();
+                    done();
+                }, 10);
+            });
+        });
+
+        describe("setPresence()", function () {
+            it("throws an error", function (done) {
+                client.setPresence({presence: 'available'}).done(function () {
+                    done(new Error("Shouldn't be able to set presence when not connected!"));
+                }, function (err) {
+                    expect(err.message).to.contain("not connected");
+                    done();
+                });
+            });
+        });
+
+        describe("setOnline()", function () {
+            it("throws an error", function (done) {
+                client.setOnline().done(function () {
+                    done(new Error("Shouldn't be able to set presence when not connected!"));
+                }, function (err) {
+                    expect(err.message).to.contain("not connected");
+                    done();
+                });
+            });
+        });
+
+        describe("sendMessage()", function () {
+            it("throws an error", function (done) {
+                client.sendMessage({
+                    endpointId: brightstream.makeGUID(),
+                    message: "The rent is TOO DAMN HIGH!"
+                }).done(function () {
+                    done(new Error("Shouldn't be able to send a message when not connected!"));
+                }, function (err) {
+                    expect(err.message).to.contain("not connected");
+                    done();
+                });
+            });
+        });
+
+        describe("startCall()", function () {
+            it("throws an error", function (done) {
+                client.startCall({
+                    endpointId: brightstream.makeGUID()
+                }).done(function () {
+                    done(new Error("Shouldn't be able to start a call when not connected!"));
+                }, function (err) {
+                    expect(err.message).to.contain("not connected");
+                    done();
+                });
+            });
+        });
+
+        describe("join()", function () {
+            it("throws an error", function (done) {
+                expect(typeof client.join).to.equal('function');
+                try {
+                client.join({
+                    id: brightstream.makeGUID()
+                }).done(function () {
+                    done(new Error("Shouldn't be able to join a group when not connected!"));
+                }, function (err) {
+                    expect(err.message).to.contain("not connected");
+                    done();
+                });
+                } catch(e) {
+                    expect(e ? e.stack: e).to.be.undefined;
+                }
+            });
+        });
+
+        describe("getGroup()", function () {
+            it("returns undefined", function () {
+                var group = client.getGroup({
+                    id: brightstream.makeGUID()
+                });
+                expect(group).to.equal.undefined;
             });
         });
     });
