respoke/respoke

commit 49447e011fec9d87f1b9fe1856bd74a36b7950b0

Don't offer to receive audio on a data connection
When creating a direct connection, WebRTC implicitly offers to accepts
audio unless you explicitly tell it not to. In
chrome://webrtc-internals/, you could see Chrome allocating audio
resources, even though we never used them.

This patch also addresses an issue with connectionSettings not be passed
onto the call for direct connections.

MER-970 #close

respoke/endpoints.js
@@ -477,11 +477,11 @@ respoke.Endpoint = function (params) {
      * directly to the other endpoint.
      */
     that.startDirectConnection = function (params) {
-        params = params || {};
         var combinedConnectionSettings = clone(client.callSettings);
         var deferred = Q.defer();
         var retVal = respoke.handlePromise(deferred.promise, params.onSuccess, params.onError);
         var call;
+        params = params || {};
 
         try {
             client.verifyConnected();
@@ -505,10 +505,11 @@ respoke.Endpoint = function (params) {
             return retVal;
         }
 
-        // Apply connection-specific callSettings to the app's defaults
+        // Apply connection-specific connectionSettings to the app's defaults
+        combinedConnectionSettings.constraints = params.constraints || combinedConnectionSettings.constraints;
         combinedConnectionSettings.servers = params.servers || combinedConnectionSettings.servers;
 
-        params.connectionSettings = combinedConnectionSettings;
+        params.callSettings = combinedConnectionSettings;
         params.instanceId = instanceId;
         params.remoteEndpoint = that;
 
@@ -558,6 +559,12 @@ respoke.Endpoint = function (params) {
             log.debug(signalParams.report);
         };
         params.directConnectionOnly = true;
+        // Don't include audio in the offer SDP
+        params.offerOptions = {
+            mandatory: {
+                OfferToReceiveAudio: false
+            }
+        };
 
         call = respoke.Call(params);
         call.listen('direct-connection', function directConnectionHandler(evt) {

respoke/call.js
@@ -263,7 +263,7 @@ respoke.Call = function (params) {
                 { RtpDataChannels: false }
             ]
         },
-        offerOptions: null,
+        offerOptions: params.offerOptions || null,
         signalOffer: params.signalOffer,
         signalConnected: params.signalConnected,
         signalAnswer: params.signalAnswer,

commit da3b9e20bef80fe9078d6b44a40e3bb32b74142d

Fix params undefined bug introduced in 49447e01.
params was undefined in call to handlePromise due to the definition
check being moved below the var statements.

respoke/endpoints.js
@@ -480,11 +480,11 @@ respoke.Endpoint = function (params) {
      * directly to the other endpoint.
      */
     that.startDirectConnection = function (params) {
+        params = params || {};
         var combinedConnectionSettings = clone(client.callSettings);
         var deferred = Q.defer();
         var retVal = respoke.handlePromise(deferred.promise, params.onSuccess, params.onError);
         var call;
-        params = params || {};
 
         try {
             client.verifyConnected();