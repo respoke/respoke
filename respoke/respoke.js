c248e95b257a4cb3be11ea59e4ee6b7e505e6c64
Wayne Schroer <wayneschroer@gmail.com>
2014-08-14 13:25:47 -0500

    Make sure only respoke.js file errors are sent to Airbrake

--- respoke/respoke.js
+++ respoke/respoke.js
@@ -62,7 +62,10 @@ if (!window.skipBugsnag) {
     airbrake.setAttribute('data-airbrake-project-environment-name', 'production');
 
     window.onerror = function(message, file, line) {
-        Airbrake.push({error: {message: message, fileName: file, lineNumber: line}});
+        //Only send errors from the respoke.js file to Airbrake
+        if (file.match(/respoke/)) {
+            Airbrake.push({error: {message: message, fileName: file, lineNumber: line}});
+        }
     }
 }
 
