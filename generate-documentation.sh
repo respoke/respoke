0d51ad8d659806155f8eb27042d9d05419bccf26
Jeff Parrish <jeffhparrish@gmail.com>
2014-11-18 08:45:24 -0800

    removing files which are not in use from old docs process. keep minified release files.

--- generate-documentation.sh
+++
@@ -1,1 +0,0 @@
-jsdoc -p -d confluence -t /usr/local/bin/jsdoc/templates/confluence-jsdoc-template respoke.js respoke/event.js respoke/client.js respoke/endpoints.js respoke/signalingChannel.js respoke/call.js respoke/directConnection.js respoke/mediaStats.js respoke/peerConnection.js respoke/localMedia.js
