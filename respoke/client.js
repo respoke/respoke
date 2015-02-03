454198cde4d8a056df68d48bf70a5cbdf5ee0810
Chad McElligott <cmcelligott@digium.com>
2015-02-03 16:37:46 -0600

    fixing issue with auto reconnect when using constructor for passing developmentMode param

--- respoke/client.js
+++ respoke/client.js
@@ -235,8 +235,10 @@ module.exports = function (params) {
         clientSettings.enableCallDebugReport = typeof clientSettings.enableCallDebugReport === 'boolean' ?
             clientSettings.enableCallDebugReport : true;
 
+
+
         if (typeof params.reconnect !== 'boolean') {
-            clientSettings.reconnect = typeof params.developmentMode === 'boolean' ? params.developmentMode : false;
+            clientSettings.reconnect = typeof clientSettings.developmentMode === 'boolean' ? clientSettings.developmentMode : false;
         } else {
             clientSettings.reconnect = !!params.reconnect;
         }
