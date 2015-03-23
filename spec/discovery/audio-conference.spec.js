f7b97ff7a592cbc36c438c88ad128f3ec7219738
Erin Spiceland <espiceland@digium.com>
2015-03-23 11:25:09 -0500

    Do a check to print a helpful message when discovery test not configured.

--- spec/discovery/audio-conference.spec.js
+++ spec/discovery/audio-conference.spec.js
@@ -11,6 +11,12 @@ describe("Respoke audio conferencing", function () {
     var appSecret = "";
     var roleId = "";
 
+    it("is configured", function () {
+        expect(appId).not.to.equal("");
+        expect(appSecret).not.to.equal("");
+        expect(roleId).not.to.equal("");
+    });
+
     function request(params, callback) {
         var xhr = new XMLHttpRequest();
         var paramString;
@@ -63,83 +69,85 @@ describe("Respoke audio conferencing", function () {
         };
     }
 
-    beforeEach(function (done) {
-        request({
-            method: "POST",
-            path: "/v1/tokens",
-            appSecret: true,
-            parameters: {
-                appId: appId,
-                endpointId: "test",
-                roleId: roleId,
-                ttl: 84600
-            }
-        }, function (err, response) {
-            if (err) {
-                done(err);
-                return;
-            }
-
-            client = respoke.createClient();
-            client.connect({
-                appId: appId,
-                baseURL: baseURL,
-                token:  response.result.tokenId
-            }).done(function () {
-                expect(client.endpointId).not.to.be.undefined;
-                expect(client.endpointId).to.equal("test");
-                done();
-            }, done);
-        });
-    });
-
-    afterEach(function (done) {
-        conf.listen('hangup', function () {
-            client.disconnect().fin(function () {
-                done();
-            }).done();
-        });
-        conf.hangup();
-    });
-
-    describe("when placing a call", function () {
-        this.timeout(30*60*60*1000);
-        var localMedia;
-        var remoteMedia;
-
+    describe('when the test is configured', function () {
         beforeEach(function (done) {
-            var doneOnce = doneOnceBuilder(done);
-
-            conf = client.startConferenceCall({
-                conferenceId: "conference-service",
-                open: true,
-                onLocalMedia: function (evt) {
-                    localMedia = evt.stream;
-                },
-                onConnect: function (evt) {
-                    doneOnce();
-                },
-                onHangup: function (evt) {
-                    doneOnce(new Error("Call got hung up"));
+            request({
+                method: "POST",
+                path: "/v1/tokens",
+                appSecret: true,
+                parameters: {
+                    appId: appId,
+                    endpointId: "test",
+                    roleId: roleId,
+                    ttl: 84600
                 }
+            }, function (err, response) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                client = respoke.createClient();
+                client.connect({
+                    appId: appId,
+                    baseURL: baseURL,
+                    token:  response.result.tokenId
+                }).done(function () {
+                    expect(client.endpointId).not.to.be.undefined;
+                    expect(client.endpointId).to.equal("test");
+                    done();
+                }, done);
             });
         });
 
-        it("succeeds and sets up outgoingMedia", function () {
-            expect(localMedia).to.be.ok;
-            expect(conf.call.outgoingMediaStreams.length).to.equal(1);
-            expect(conf.call.incomingMediaStreams.length).to.equal(1);
-            expect(conf.call.outgoingMedia).to.be.ok;
-            expect(conf.call.outgoingMedia.className).to.equal('respoke.LocalMedia');
-            expect(conf.call.incomingMedia).to.be.ok;
-            expect(conf.call.incomingMedia.className).to.equal('respoke.RemoteMedia');
-            expect(conf.call.outgoingMedia.hasVideo()).to.equal(false);
-            expect(conf.call.outgoingMedia.hasAudio()).to.equal(true);
-            expect(conf.call.incomingMedia.hasVideo()).to.equal(false);
-            expect(conf.call.incomingMedia.hasAudio()).to.equal(true);
-            expect(conf.call.hasMedia()).to.equal(true);
-            expect(conf.call.hasAudio).to.equal(true);
-            expect(conf.call.hasVideo).to.equal(false);
+        afterEach(function (done) {
+            conf.listen('hangup', function () {
+                client.disconnect().fin(function () {
+                    done();
+                }).done();
+            });
+            conf.hangup();
+        });
+
+        describe("when placing a call", function () {
+        this.timeout(30*60*60*1000);
+            var localMedia;
+            var remoteMedia;
+
+            beforeEach(function (done) {
+                var doneOnce = doneOnceBuilder(done);
+
+                conf = client.startConferenceCall({
+                    conferenceId: "conference-service",
+                    open: true,
+                    onLocalMedia: function (evt) {
+                        localMedia = evt.stream;
+                    },
+                    onConnect: function (evt) {
+                        doneOnce();
+                    },
+                    onHangup: function (evt) {
+                        doneOnce(new Error("Call got hung up"));
+                    }
+                });
+            });
+
+            it("succeeds and sets up outgoingMedia", function () {
+                expect(localMedia).to.be.ok;
+                expect(conf.call.outgoingMediaStreams.length).to.equal(1);
+                expect(conf.call.incomingMediaStreams.length).to.equal(1);
+                expect(conf.call.outgoingMedia).to.be.ok;
+                expect(conf.call.outgoingMedia.className).to.equal('respoke.LocalMedia');
+                expect(conf.call.incomingMedia).to.be.ok;
+                expect(conf.call.incomingMedia.className).to.equal('respoke.RemoteMedia');
+                expect(conf.call.outgoingMedia.hasVideo()).to.equal(false);
+                expect(conf.call.outgoingMedia.hasAudio()).to.equal(true);
+                expect(conf.call.incomingMedia.hasVideo()).to.equal(false);
+                expect(conf.call.incomingMedia.hasAudio()).to.equal(true);
+                expect(conf.call.hasMedia()).to.equal(true);
+                expect(conf.call.hasAudio).to.equal(true);
+                expect(conf.call.hasVideo).to.equal(false);
+            });
         });
     });
 });
