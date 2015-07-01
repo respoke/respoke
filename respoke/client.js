b4d578deb5b53e44f1e4ea3a133205aa13cf566c
Erin Spiceland <espiceland@digium.com>
2015-07-01 16:27:57 -0500

    Handle outgoing SIP caller ID just like we do with phone calls.

--- respoke/client.js
+++ respoke/client.js
@@ -1432,6 +1432,9 @@ module.exports = function (params) {
      * @param {string} [params.user] - The SIP user to call. This is not necessary if `uri` is set. If `uri` is not
      * set, both `trunk` and `user` are required, and `trunk` must be the ID of a Respoke SIP trunk. `user` is a
      * SIP username or extension.
+     * @param {object} [params.callerId] - Caller ID information for this call.
+     * @param {string} [params.callerId.name] - Caller ID name.
+     * @param {string} [params.callerId.number] - Caller ID number, extension, or SIP username.
      * @param {respoke.Call.onLocalMedia} [params.onLocalMedia] - Callback for receiving an HTML5 Video element
      * with the local audio and/or video attached.
      * @param {respoke.Call.onError} [params.onError] - Callback for errors that happen during call setup or
@@ -1502,6 +1505,11 @@ module.exports = function (params) {
             signalParams.recipient = recipient;
             signalParams.toType = params.toType;
             signalParams.fromType = params.fromType;
+
+            // using hasOwnProperty here because callerId could be explicitly set to null or empty string
+            if (params.hasOwnProperty('callerId')) {
+                signalParams.callerId = params.callerId;
+            }
             signalingChannel.sendSDP(signalParams).done(onSuccess, onError);
         };
         params.signalAnswer = function (signalParams) {
